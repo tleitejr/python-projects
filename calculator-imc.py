7af23841429442c50a62973bdeec79f31ad5a4f3
Antonio Leite Jr <88110285+tleitejr@users.noreply.github.com>
2023-04-29 14:46:13 -0300

    Calculadora de IMC
    Calculadora de IMC (Índice de Massa Corporal) em python.

---
+++ calculator-imc.py
@@ -0,0 +1,21 @@
+def resultado():
+  if imc < 18.5:
+    print('Resultado: MAGREZA')
+  elif imc > 18.5 and imc < 24.9:
+    print('Resultado: NORMAL')
+  elif imc > 25.0 and imc < 29.9:
+    print('Resultado: SOBREPESO')
+  elif imc < 30.0 and imc < 39.9:
+    print('Resultado: OBESIDADE')
+  elif imc >= 40.0:
+    print('Resultado: OBESIDADE GRAVE')
+  else:
+    print('VALOR INCORRETO!')
+
+peso = float(input('Digite o seu peso: '))
+altura = float(input('Digite a sua altura: '))
+imc = peso / (altura * altura)
+print('')
+print('IMC: {}'.format(imc))
+print('')
+resultado()
